4d1abb6641446f82ebf5337402ef2cc5ad23380d
hishow1996 <125007127+hishow1996@users.noreply.github.com>
2026-09-05 10:54:18 +0800

    chore: bump app version to 1.5.1

--- app/src/main/java/com/hishow/terminal33/BuildInfo.kt
+++ app/src/main/java/com/hishow/terminal33/BuildInfo.kt
@@ -1,7 +1,7 @@
 package com.hishow.terminal33
 
 object BuildInfo {
-    const val VERSION_NAME = "1.5.0"
+    const val VERSION_NAME = "1.5.1"
     const val TARGET_ABI = "arm64-v8a"
     const val APP_NAME = "Luma"
 }
