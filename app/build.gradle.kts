c3fbdcc563d5a73d02febc41fb5124397161c5a0
hishow1996 <125007127+hishow1996@users.noreply.github.com>
2026-09-05 10:14:16 +0800

    chore: bump Android version to 1.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.hishow.terminal33"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.4.0"
+        versionCode = 5
+        versionName = "1.5.0"
         ndk { abiFilters += "arm64-v8a" }
     }
 
